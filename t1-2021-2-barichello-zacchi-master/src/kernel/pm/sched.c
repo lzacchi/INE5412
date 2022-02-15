d5beef387130ac7b0a37ee3388eecb6a1e685493
Lucas Zacchi <zacchilucasm@gmail.com>
2022-02-15 10:46:15 -0300

    Add t1

---
+++ t1-2021-2-barichello-zacchi-master/src/kernel/pm/sched.c
@@ -0,0 +1,147 @@
+/*
+ * Copyright(C) 2011-2016 Pedro H. Penna   <pedrohenriquepenna@gmail.com>
+ *              2015-2016 Davidson Francis <davidsondfgl@hotmail.com>
+ *
+ * This file is part of Nanvix.
+ *
+ * Nanvix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nanvix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <nanvix/clock.h>
+#include <nanvix/const.h>
+#include <nanvix/hal.h>
+#include <nanvix/pm.h>
+#include <nanvix/klib.h>
+#include <signal.h>
+
+int TOTAL_TICKETS = 0;
+
+/**
+ * @brief Inflate lottery tickets based on QUANTUM usage.
+ * 
+ * @param proc Process to be analyzed.
+ */
+PUBLIC void ticket_compensation(struct process *proc)
+{
+	if (proc->counter > 0 && proc->counter < PROC_QUANTUM)
+	{
+		int ticket_inflation = PROC_QUANTUM / proc->counter;
+		proc->tickets *= ticket_inflation;
+	}
+}
+
+/**
+ * @brief Schedules a process to execution.
+ * 
+ * @param proc Process to be scheduled.
+ */
+PUBLIC void sched(struct process *proc)
+{
+	proc->state = PROC_READY;
+	proc->tickets += 10;
+	TOTAL_TICKETS += proc->tickets;
+	ticket_compensation(proc);
+	proc->counter = 0;
+}
+
+/**
+ * @brief Stops the current running process.
+ */
+PUBLIC void stop(void)
+{
+	curr_proc->state = PROC_STOPPED;
+	sndsig(curr_proc->father, SIGCHLD);
+	yield();
+}
+
+/**
+ * @brief Resumes a process.
+ * 
+ * @param proc Process to be resumed.
+ * 
+ * @note The process must stopped to be resumed.
+ */
+PUBLIC void resume(struct process *proc)
+{	
+	/* Resume only if process has stopped. */
+	if (proc->state == PROC_STOPPED)
+		sched(proc);
+}
+
+/**
+ * @brief Lottery scheduling function.
+ * @return returns the lottery winning process
+ */
+PUBLIC struct process* lottery()
+{
+	int random_ticket = krand_range(TOTAL_TICKETS);
+	int accumulated_tickets = 0;
+	struct process *p;
+	for (p = FIRST_PROC; p <= LAST_PROC; p++)
+	{
+		/* Skip non-ready process. */
+		if (p->state != PROC_READY)
+			continue;
+
+		accumulated_tickets += p->tickets;
+		if (accumulated_tickets > random_ticket)
+		{
+			/* Winner process has been found */	
+			return p;
+		}
+	}
+	return IDLE;
+}
+
+/**
+ * @brief Yields the processor.
+ */
+PUBLIC void yield(void)
+{
+	struct process *p;    /* Working process.     */
+	struct process *next; /* Next process to run. */
+
+	/* Re-schedule process for execution. */
+	if (curr_proc->state == PROC_RUNNING)
+		sched(curr_proc);
+
+	/* Remember this process. */
+	last_proc = curr_proc;
+
+	/* Check alarm and sum total tickets */
+	for (p = FIRST_PROC; p <= LAST_PROC; p++)
+	{
+		/* Skip invalid processes */
+		if (!IS_VALID(p))
+			continue;
+	
+		/* Alarm has expired. */
+		if ((p->alarm) && (p->alarm < ticks))
+			p->alarm = 0, sndsig(p, SIGALRM);
+	}
+
+	/* Choose a process to run next. */
+	next = IDLE;
+	if (TOTAL_TICKETS > 0)
+	{
+		/* Draws a winning ticket */
+		next = lottery();
+	}
+	/* Switch to next process. */
+	next->priority = PRIO_USER;
+	next->state = PROC_RUNNING;
+	next->counter = PROC_QUANTUM;
+	if (curr_proc != next)
+		switch_to(next);
+}
